92c1edebd12ad4c88edf4db7aa2208be2e04c872
duyufeng <duyufeng@zhuanzhuan.com>
2021-06-15 19:32:48 +0800

    add:新建项目结构

---
+++ zrrpc/src/main/java/com/zr/zrrpc/Main.java
@@ -0,0 +1,16 @@
+package com.zr.zrrpc;
+
+/**
+ * <h3>ZR-RPC</h3>
+ * <h4>com.zr.zrrpc</h4>
+ * <p></p>
+ *
+ * @author : richlogic
+ * @since : 2021/06/15
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
